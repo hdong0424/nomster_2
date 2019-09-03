d2b752ce0846b942de1b177eeb6a179ff9db5fcb
donghanbing1989@gmail.com <donghanbing1989@gmail.com>
2019-09-03 23:03:04 +0000

    Lesson 28

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,13 @@
 class Place < ApplicationRecord
 	belongs_to :user
-	validates :name, presence: true
+	validates :name, :address, :description, presence: true
+	validates :name, length: { minimum: 4 }
+	validates :description, length: {maximum: 500}
 end
+#additional validation
+class Person < ApplicationRecord
+	validates :email, confirmation: true
+	validates :email_confirmation, presence: true
+	validates :password, length: {in: 6..20}
+end
+
