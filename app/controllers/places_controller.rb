7ab3f04fc1504ebebfed833ca478db209fcbe69c
donghanbing1989@gmail.com <donghanbing1989@gmail.com>
2019-09-04 23:35:58 +0000

    Lesson 30

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 	def index
 		@places = Place.all
@@ -26,7 +26,7 @@ class PlacesController < ApplicationController
 		@place = Place.find(params[:id])
 
 		if @place.user != current_user
-			return render plaun: 'Not Allowed', status: :forbidden
+			return render plain: 'Not Allowed', status: :forbidden
 		end
 	end
 
@@ -46,6 +46,10 @@ class PlacesController < ApplicationController
 
 	def destroy
 		@place = Place.find(params[:id])
+		if @palce.user !=current_user
+			return render plain: 'Not Allowed', status: :forbidden
+		end
+		
 		@place.destroy
 		redirect_to root_path
 	end
