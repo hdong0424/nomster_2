b63b3c43bdd885509988862b49eef6653dcae984
donghanbing1989@gmail.com <donghanbing1989@gmail.com>
2019-09-15 18:41:33 +0000

    Picutre

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,13 +3,14 @@ class PhotosController < ApplicationController
 
 	def create
 		@place = Place.find(params[:place_id])
-		@place.photos.create()
+		@place.photos.create(photo_params.merge(user: current_user))
 		redirect_to place_path(@place)
 	end
 
 	private
 
 	def photo_params
-		params.require(:photo).permit(:caption)
+		params.require(:photo).permit(:caption, :picture)
 	end
+
 end
