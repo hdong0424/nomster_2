591253e00980f7de98e5e457d6c13f628f99b895
donghanbing1989@gmail.com <donghanbing1989@gmail.com>
2019-09-18 03:08:40 +0000

    Lesson 43

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2019_09_15_185042) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_places_on_user_id"
